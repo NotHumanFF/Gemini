15c17a6dfcf81f3f9f976b367dfa00274a793f5d
Parth Awasthi <107567458+NotHumanFF@users.noreply.github.com>
2023-12-15 11:24:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
-app.get("/home", function (req, res) {
+app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
